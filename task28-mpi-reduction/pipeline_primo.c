a2cb16a1b5717bb6d2b1c8a9a57a02ac8631388a
axell-brendow <breno.axel@gmail.com>
2020-11-05 12:15:22 -0300

    Finish task28-mpi-reduction

--- task28-mpi-reduction/pipeline_primo.c
+++ task28-mpi-reduction/pipeline_primo.c
@@ -16,8 +16,6 @@ int rank;
 
 void removeMultipleOf3()
 {
-  printf("a\n");
-  exit(0);
   int number, i;
 
   for (i = 1; i <= n / 2; i++)
@@ -28,13 +26,12 @@ void removeMultipleOf3()
     if (number % 3 > 0)
       MPI_Send(&number, 1, MPI_INT, 1, PIPE_MSG, MPI_COMM_WORLD);
   }
+
   MPI_Send(&number, 1, MPI_INT, 1, END_MSG, MPI_COMM_WORLD);
 }
 
 void removeMultipleOf5()
 {
-  printf("a\n");
-  exit(0);
   int number;
   MPI_Status Status;
 
@@ -51,33 +48,27 @@ void removeMultipleOf5()
       // enviar para o próximo estágio
       MPI_Send(&number, 1, MPI_INT, 2 + index, PIPE_MSG, MPI_COMM_WORLD);
       index = (index + 1) % (size - 2);
-      printf("%d\n", index + 2);
-      exit(0);
     }
   }
-  printf("a\n");
-  exit(0);
+
   for (int i = 2; i < size; i++)
   {
     // enviar mensagem de finalização
     MPI_Send(&number, 1, MPI_INT, i, END_MSG, MPI_COMM_WORLD);
   }
-  printf("b\n");
-  exit(0);
+
   for (int i = 2; i < size; i++)
   {
     // receber mensagem do terceiro estagio
     MPI_Recv(&number, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
     primeCountTotal += number;
   }
+
   printf("number of primes = %d\n", primeCountTotal);
 }
 
 void countOnlyPrimes()
 {
-  printf("a\n");
-  exit(0);
-
   int number, primeCount, i, isComposite;
   MPI_Status Status;
 
@@ -103,7 +94,6 @@ void countOnlyPrimes()
       primeCount++;
   }
 
-  // printf("number of primes = %d\n",primeCount);
   MPI_Send(&primeCount, 1, MPI_INT, 1, END_MSG, MPI_COMM_WORLD);
 }
 
