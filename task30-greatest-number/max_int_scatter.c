898112173ca2880fa8e8c252b177db13b8e640bd
axell-brendow <breno.axel@gmail.com>
2020-11-12 11:55:17 -0300

    Finish task30-greatest-number

--- task30-greatest-number/max_int_scatter.c
+++ task30-greatest-number/max_int_scatter.c
@@ -1,7 +1,6 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <mpi.h>
 
 #define N 10
@@ -18,40 +17,34 @@ void main(int argc, char* argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
 
+  int numElementsPerProcess = (N / numProcs) + 1;
+  int nums[numElementsPerProcess];
+
   if (rank == 0) {
     // preencher o buffer com N valores inteiros aleatórios
-    for (size_t i = 0; i < N; i++) {
-      buffer[i] = rand();
-      printf("buffer[%d] = %d\n", i, buffer[i]);
-    }
+    for (size_t i = 0; i < N; i++) buffer[i] = rand();
   } 
 
   // distribuir o vetor para todos os outros processos
-  MPI_Bcast(
+  MPI_Scatter(
       buffer,
-      N,
+      numElementsPerProcess,
+      MPI_INT,
+      &nums,
+      numElementsPerProcess,
       MPI_INT,
       0,
       MPI_COMM_WORLD);
 
-  // processar o maior dos valores dentro do seu intervalo
-  int greatest = INT_MIN;
+  for (int i = 0; i < numElementsPerProcess; i++)
+    printf("rank = %d, value[i] = %d\n", rank, nums[i]);
 
-  for (int i = rank; i < N; i += numProcs)
-    greatest = buffer[i] > greatest ? buffer[i] : greatest;
+  // processar o maior dos valores dentro do seu intervalo
 
   // reduzir os maiores no maior, enviando o resultado para o processo com rank = 0
-  MPI_Reduce(
-    &greatest,
-    &maior_final,
-    1,
-    MPI_INT,
-    MPI_MAX,
-    0,
-    MPI_COMM_WORLD);
 
   if (rank == 0) {
-    printf("maior_final = %d\n", maior_final);
+    // imprimir maior
   }
   
   MPI_Finalize();
